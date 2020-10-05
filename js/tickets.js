2f8ee0352a50c1194c9318b166759b8f7c370637
balabolchik <obordulyak@gmail.com>
2020-10-06 01:36:38 +0300

    preFinish

---
+++ js/tickets.js
@@ -0,0 +1,24 @@
+const container = document.querySelector('.holl');
+const seats = document.querySelectorAll('.rows .seat:not(occupied)');
+let total = document.querySelector('.total');
+const price = document.querySelector('.price');
+const button = document.querySelector('.end');
+
+const ticketPrice = price.id;
+
+let i =0;
+
+container.addEventListener('click', e => {
+    if (e.target.classList.contains('seat') && !e.target.classList.contains('selected') && !e.target.classList.contains('occupied')) {
+        e.target.classList.toggle('selected');
+        i+=1;
+        document.getElementById('total').innerHTML=i*ticketPrice;
+    }
+});
+
+button.addEventListener('click', () => {
+    document.querySelectorAll('.selected').classList.remove('selected');
+    console.log(document.querySelectorAll('.selected'));//.classList.remove('selected').classList.add('occupied');
+    console.log(i);
+    document.getElementById('total').innerHTML = 0;
+});
