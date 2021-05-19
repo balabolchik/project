45d368f7a0be645914b24992f6d0e11cf7ba2ea6
Balabolchik <obordulyak@gmail.com>
2021-05-19 18:57:54 +0300

    added a booking of the place

--- js/tickets.js
+++ js/tickets.js
@@ -1,21 +1,28 @@
-const container = document.querySelector('.holl');
-const seats = document.querySelectorAll('.rows .seat:not(occupied)');
-let total = document.querySelector('.total');
-const price = document.querySelector('.price');
-const button = document.querySelector('.end');
+const container = document.querySelector(".holl");
+const seats = document.querySelectorAll(".rows .seat:not(occupied)");
+let total = document.querySelector(".total");
+const price = document.querySelector(".price");
+const button = document.querySelector(".end");
 
 const ticketPrice = price.id;
 
-let i =0;
+let i = 0;
 
-container.addEventListener('click', e => {
-    if (e.target.classList.contains('seat') && !e.target.classList.contains('selected') && !e.target.classList.contains('occupied')) {
-        e.target.classList.toggle('selected');
-        i+=1;
-        document.getElementById('total').innerHTML=i*ticketPrice;
+container.addEventListener("click", (e) => {
+    if (
+        e.target.classList.contains("seat") &&
+        !e.target.classList.contains("selected") &&
+        !e.target.classList.contains("occupied")
+    ) {
+        e.target.classList.toggle("selected");
+        i += 1;
+        document.getElementById("total").innerHTML = i * ticketPrice;
     }
 });
 
-button.addEventListener('click', () => {
-    document.getElementById('total').innerHTML = 0;
+button.addEventListener("click", () => {
+    document.getElementById("total").innerHTML = 0;
+    let seats = document.querySelectorAll(".selected");
+    seats.forEach((el) => el.classList.add("occupied"));
+    seats.forEach((el) => el.classList.toggle("selected"));
 });
